02f1f452b946ab6837d68d32bcb53bf5cb45974d
Adrian Rubio-Punal <adrian.rubio.punal@gmail.com>
2025-01-23 20:25:03 +0100

    Auto commit on jue 23 ene 2025 20:25:03 CET

--- CNN-object-detection.py
+++ CNN-object-detection.py
@@ -1,10 +1,17 @@
 # CNN
 import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader
 from transformers import AutoFeatureExtractor, AutoModelForObjectDetection
+from datasets import load_dataset
 import numpy as np
 import matplotlib as plt
 from datetime import datetime
 
+# Load COCO dataset
+train_dataset = load_dataset('coco_detection', 'train')
+test_dataset = load_dataset('coco_detection', 'train')
+
+# Load the feature extractor
 feature_extractor = AutoFeatureExtractor.from_pretrained('facebook/detr-resnet-50')
-model = AutoModelForObjectDetection.from_pretrained('facebook/detr-resnet-50')
+
