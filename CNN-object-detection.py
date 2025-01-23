dab3cdc532d010e46d719788cdc3ab54091b0863
Adrian Rubio-Punal <adrian.rubio.punal@gmail.com>
2025-01-23 20:20:03 +0100

    Auto commit on jue 23 ene 2025 20:20:03 CET

--- CNN-object-detection.py
+++ CNN-object-detection.py
@@ -1,1 +1,10 @@
 # CNN
+import torch
+import torch.nn as nn
+from transformers import AutoFeatureExtractor, AutoModelForObjectDetection
+import numpy as np
+import matplotlib as plt
+from datetime import datetime
+
+feature_extractor = AutoFeatureExtractor.from_pretrained('facebook/detr-resnet-50')
+model = AutoModelForObjectDetection.from_pretrained('facebook/detr-resnet-50')
