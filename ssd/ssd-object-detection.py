e6a81b2379bff7459eeeae0875aa53d55e03d573
Adrian Rubio-Punal <adrian.rubio.punal@gmail.com>
2025-03-20 20:30:03 +0100

    Auto commit on jue 20 mar 2025 20:30:03 CET

--- ssd/ssd-object-detection.py
+++ ssd/ssd-object-detection.py
@@ -2,6 +2,7 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+import torchvision
 import torchvision.transforms as transforms
 from transformers import AutoFeatureExtractor, AutoModelForObjectDetection
 from datasets import load_dataset
